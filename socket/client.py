d80b14148a009cbcf9a3990aae8f304b7a9fb014
Ketan Pujare <ketanpujare@gmail.com>
2019-07-18 15:40:57 +0530

    networking

---
+++ socket/client.py
@@ -0,0 +1,35 @@
+# #!/usr/bin/env python3
+
+# import socket
+
+# HOST = '127.0.0.1'  # The server's hostname or IP address
+# PORT = 65432        # The port used by the server
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((HOST, PORT))
+#     s.sendall(b'Hello, world')
+#     data = s.recv(1024)
+
+# print('Received', repr(data))
+
+# multi_threaded.py
+import time
+from threading import Thread
+
+COUNT = 50000000
+
+def countdown(n):
+    while n>0:
+        n -= 1
+
+t1 = Thread(target=countdown, args=(COUNT//2,))
+t2 = Thread(target=countdown, args=(COUNT//2,))
+
+start = time.time()
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+end = time.time()
+
+print('Time taken in seconds -', end - start)
